8d6bc2bd3e902cc8afdbba1d95b8c82c14224632
dimasConfig <31501336+iCe27conf@users.noreply.github.com>
2025-08-21 05:16:04 +0700

    Create index.php
    File Index.php perttama

---
+++ index.php
@@ -0,0 +1,473 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="theme-color" content="#75dab4"/>
+<title>Cretechst | Studio</title>
+<meta name="author" content="dMz Ismu">
+<meta name="description" content="Cretechst | Studio">
+<meta name="robots" content="index, follow">
+<meta name="keywords" content="Cretechst, dMzismu, Music Cretechst, Best Song Cretechst, web development, mobile app development, PHP, Python, Flutter, Canada Jakarta developer, Sydney developer, Singapure developer, Japan developer, Jakarta developer, custom software, Indonesia tech studio, Kreasi Teknologi Internasional, Creative Technologi">
+
+
+<!-- SOCIAL MEDIA META -->
+<meta property="og:description" content="Cretechst | Studio">
+<meta property="og:image" content="http://cretechst.ct.ws/images/logo.png">
+<meta property="og:site_name" content="Cretechst">
+<meta property="og:title" content="Cretechst">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://cretechst.ct.ws">
+
+<!-- TWITTER META -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@Cretechst">
+<meta name="twitter:creator" content="@Cretechst">
+<meta name="twitter:title" content="Cretechst">
+<meta name="twitter:description" content="Cretechst | Studio">
+<meta name="twitter:image" content="http://cretechst.ct.ws/images/logo.png">
+
+<!-- FAVICON FILES -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+<link href="ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
+<link href="ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+<link href="ico/favicon.png" rel="shortcut icon">
+
+<!-- CSS FILES -->
+<link rel="stylesheet" href="css/fontawesome.min.css">
+<link rel="stylesheet" href="css/fancybox.min.css">
+<link rel="stylesheet" href="css/hamburger.min.css">
+<link rel="stylesheet" href="css/odometer.min.css">
+<link rel="stylesheet" href="css/swiper.min.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+
+
+
+
+
+
+<body>
+<div class="preloader">
+  <div class="layer"></div>
+  <div class="inner">
+    <figure> <img src="images/preloader.gif" alt="Image"> </figure>
+    <span>CRETECHST | WAIT | LOADING</span> </div>
+</div>
+
+
+<div class="page-transition">
+  <div class="layer"></div>
+</div>
+<!-- end page-transition -->
+
+
+
+<nav class="site-navigation">
+  <div class="layer"></div>
+  <div class="inner">
+    <ul data-splitting>
+      <li><a href="index.html">HOME</a><i class="fas fa-caret-down"></i> <small>First page</small>
+      <ul>
+      	<li><a href="index.html">HOME SLIDER</a></li>
+      	<!-- <li><a href="index-video.html">HOME VIDEO</a></li>
+      	<li><a href="index-carousel.html">HOME CAROUSEL</a></li> -->
+      </ul>
+        </li>
+      <!-- <li><a href="studio.html">STUDIO</a> <small>All About Us</small> </li>
+      <li><a href="showcases.html">SHOWCASES</a> <small>Our all projects</small> </li>
+      <li><a href="blog.html">BLOG</a> <small>Recent posts</small> </li>
+      <li><a href="contact.html">CONTACT</a> <small>Say hello</small> </li> -->
+    </ul>
+  </div>]
+</nav>
+
+
+
+
+
+
+
+
+
+<div class="social-media">
+  <div class="layer"> </div>
+  <div class="inner">
+    <h5>Social Share </h5>
+    <ul>
+      <li><a href="https://open.spotify.com/artist/0gK4cUg10w6zPpuPwQUfge"><i class="bi bi-spotify"></i></a></li>
+      <li><a href="https://www.facebook.com/dmz.i.hazhar"><i class="fab fa-facebook-f"></i></a></li>
+      <li><a href="https://www.instagram.com/dimas.ismu/"><i class="fab fa-instagram"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/dimasismuhazhar/"><i class="fab fa-linkedin-in"></i></a></li>
+      <li><a href="https://www.tiktok.com/@dmz.ismu"><i class="bi bi-tiktok"></i></a></li>
+      <li><a href="https://www.youtube.com/@dmzismu"><i class="fab fa-youtube"></i></a></li>
+      <li><a href="https://open.spotify.com/artist/7M4Mtcha8pfif5rR9VQwrj?si=Tvd0sY7jR0abAiiP62FsIw"><i class="bi bi-spotify"></i></a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+<div class="all-cases">
+  <div class="layer"> </div>
+  <div class="inner">
+    <ul>
+      <li><a href="#">World Politic or GOD Drama</a></li>
+      <li><a href="#">Available Soon</a></li>
+      <li><a href="#">Coming Soon</a></li>
+      <li><a href="#">Launching Soon</a></li>
+      <li><a href="#">Releasing Soon</a></li>
+      <li><a href="#">Stay Tuned</a></li>
+       <li><a href="#">Coming This Season</a></li>
+      <li><a href="#">Coming This ....!</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<main>
+  <aside class="left-side">
+    <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="Image"></a> </div>
+    <div class="hamburger" id="hamburger">
+      <div class="hamburger__line hamburger__line--01">
+        <div class="hamburger__line-in hamburger__line-in--01"></div>
+      </div>
+      <div class="hamburger__line hamburger__line--02">
+        <div class="hamburger__line-in hamburger__line-in--02"></div>
+      </div>
+      <div class="hamburger__line hamburger__line--03">
+        <div class="hamburger__line-in hamburger__line-in--03"></div>
+      </div>
+      <div class="hamburger__line hamburger__line--cross01">
+        <div class="hamburger__line-in hamburger__line-in--cross01"></div>
+      </div>
+      <div class="hamburger__line hamburger__line--cross02">
+        <div class="hamburger__line-in hamburger__line-in--cross02"></div>
+      </div>
+    </div>
+    <div class="follow-us"> FOLLOW US </div>
+    <div class="equalizer"> <span></span> <span></span> <span></span> <span></span> </div>
+  </aside>
+  <div class="all-cases-link"> <span>ALL ALBUMS</span> <b>+</b> </div>
+  <header class="slider">
+    <div class="swiper-container gallery-top">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" data-background="images/slide01.jpg"></div>
+        <div class="swiper-slide">
+          <video src="videos/struggle-for-mother.mp4" muted autoplay loop></video>
+        </div>
+        <div class="swiper-slide" data-background="images/slide02.jpg"></div>
+      </div>
+      <div class="slide-progress"> <span>01</span>
+        <div class="swiper-pagination"></div>
+        <span>03</span> </div>
+      <div class="swiper-button-prev">PREV</div>
+      <div class="swiper-button-next">NEXT</div>
+    </div>
+    <div class="swiper-container gallery-thumbs">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><span>Music</span> <a href="#">CRETECHST</a> </div>
+        <div class="swiper-slide"><span>Listen</span> <a href="#">CRETECHST</a></div>
+        <div class="swiper-slide"><span>Play   .</span> <a href="#">CRETECHST</a></div>
+      </div>
+    </div>
+  </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <section class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 wow" data-splitting>
+          <h3 class="section-title">WE DON'T JUST PLAY<br>
+            WE CREATE VIBES</h3>
+          <a href="mailto:info@neonsynth.com">info@neonsynth.com</a> 
+        </div>
+        <div class="col-lg-7 wow" data-splitting>
+          <p>Where analog meets digital, and traditional melodies merge with future beats. We are the bridge between classic and modern.</p>
+          <h6>dMZ Ismu</h6>
+          <small>I am, Lyricist | Composer</small> <b>STREAM IN</b>
+          <h4>SPOTIFY, <br>
+            Youtube Music, etc</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  <section class="icon-content-block">
+    <div class="container">
+      <br><br> <br><br>  <br><br>
+      <div class="row justify-content-center">
+        <div class="col-12 wow" data-splitting>
+          <!-- JUDUL YANG LEBIH RELEVAN -->
+          <h3 class="section-title">OUR MUSIC<br>
+            & WHAT WE OFFER</h3>
+        </div>
+        <!-- end col-12 -->
+
+        <!-- BLOK 1: TENTANG MUSIK KAMI -->
+        <div class="col-lg-3 col-md-4 wow" data-splitting>
+          <div class="content-block">
+            <figure>
+              <img src="images/icon01.gif" alt="Our Original Music">
+            </figure>
+            <h6>ORIGINAL MUSIC</h6>
+            <ul>
+              <li>High-Energy Live Performances</li>
+              <li>Captivating Original Songs</li>
+              <li>Dynamic Stage Presence</li>
+              <li>Memorable Musical Experience</li>
+            </ul>
+          </div>
+        </div>
+        <!-- end col-3 -->
+
+        <!-- BLOK 2: UNTUK EVENT -->
+        <div class="col-lg-3 col-md-4 wow" data-splitting>
+          <div class="content-block selected">
+            <figure> <img src="images/icon02.png" alt="Live Events"> </figure>
+            <h6>LIVE EVENTS</h6>
+            <ul>
+              <li>Concerts & Music Festivals</li>
+              <li>Club Gigs & Bar Performances</li>
+              <li>Private Parties & Weddings</li>
+              <li>Corporate Events & Functions</li>
+            </ul>
+          </div>
+        </div>
+        <!-- end col-3 -->
+
+        <!-- BLOK 3: KOLABORASI/PRODUK -->
+        <div class="col-lg-3 col-md-4 wow" data-splitting>
+          <div class="content-block">
+            <figure>
+              <img src="images/icon03.gif" alt="Our Original Music">
+            </figure>
+            <h6>MUSIC & MERCH</h6>
+            <ul>
+              <li>Original Recordings & Albums</li>
+              <li>Digital Downloads & Streaming</li>
+              <li>Band Merchandise & Apparel</li>
+              <li>Special Collaborations</li>
+            </ul>
+          </div>
+        </div>
+        <!-- end col-3 -->
+      </div>
+    </div>
+  </section> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+  <section class="works">
+     <br><br><br><br><br><br><br><br><br><br><br><br>  
+    <ul>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works01.jpg" data-fancybox><img src="images/works01.jpg" alt="Image"></a> </figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small> </div>
+        <!-- end caption --> 
+      </li>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works02.jpg" data-fancybox><img src="images/works02.jpg" alt="Image"></a> </figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small></div>
+        <!-- end caption --> 
+      </li>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works03.jpg" data-fancybox><img src="images/works03.jpg" alt="Image"></a> </figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small> </div>
+        <!-- end caption --> 
+      </li>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works04.jpg" data-fancybox><img src="images/works04.jpg" alt="Image"></a> </figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small> </div>
+        <!-- end caption --> 
+      </li>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works05.jpg" data-fancybox><img src="images/works05.jpg" alt="Image"></a> </figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small> </div>
+        <!-- end caption --> 
+      </li>
+      <li>
+        <figure class="reveal-effect masker wow"> <a href="images/works06.jpg" data-fancybox><img src="images/works06.jpg" alt="Image"> </a></figure>
+        <div class="caption wow" data-splitting>
+          <h3>dMz Ismu</h3>
+          <small>South Jakarta, DKI Jakarta, Indonesia</small> </div>
+        <!-- end caption --> 
+      </li>
+    </ul>
+  </section>
+</main>
+
+
+
+
+
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br>  
+<footer class="footer">
+  <div class="container">
+    <br><br><br><br><br><br><br><br><br><br><br><br>  
+    <div class="row">
+      <div class="col-12">
+         <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3wf4GQfC4wVka6o1lZuHhr?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <div class="footer-bar"> 
+          <span class="copyright">© 2010 Cretechst | All Rights Reserved</span> 
+          <span class="creation">
+             <ul>
+              <li><a href="https://open.spotify.com/artist/0gK4cUg10w6zPpuPwQUfge"><i class="bi bi-spotify"></i></a></li>
+              <li><a href="https://www.facebook.com/dmz.i.hazhar"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="https://www.instagram.com/dimas.ismu/"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/dimasismuhazhar/"><i class="fab fa-linkedin-in"></i></a></li>
+              <li><a href="https://www.tiktok.com/@dmz.ismu"><i class="bi bi-tiktok"></i></a></li>
+              <li><a href="https://www.youtube.com/@dmzismu"><i class="fab fa-youtube"></i></a></li>
+              <li><a href="https://open.spotify.com/artist/7M4Mtcha8pfif5rR9VQwrj?si=Tvd0sY7jR0abAiiP62FsIw"><i class="bi bi-spotify"></i></a></li>
+            </ul>
+          </span> 
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/swiper.min.js"></script> 
+<script src="js/wow.min.js"></script> 
+<script src='js/splitting.min.js'></script> 
+<script src='js/odometer.min.js'></script> 
+<script src='js/fancybox.min.js'></script> 
+<script src="js/scripts.js"></script>
+<script>
+  $('.back-to-top').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+</script>
+</body>
+</html>
